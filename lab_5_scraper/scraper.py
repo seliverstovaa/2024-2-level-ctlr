79ef762774fa87bc00ee0fabb9688a3e9042c4a7
seliverstovaa <asya030205@gmail.com>
2025-04-09 11:55:20 +0300

    2 stages try

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -1,10 +1,43 @@
 """
 Crawler implementation.
 """
-
+import os
 # pylint: disable=too-many-arguments, too-many-instance-attributes, unused-import, undefined-variable, unused-argument
 import pathlib
 from typing import Pattern, Union
+from core_utils.config_dto import ConfigDTO
+from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
+import requests
+# from bs4 import BeautifulSoup
+# import datetime
+
+
+class IncorrectSeedURLError(Exception):
+    pass
+
+
+class NumberOfArticlesOutOfRangeError(Exception):
+    pass
+
+
+class IncorrectNumberOfArticlesError(Exception):
+    pass
+
+
+class IncorrectHeadersError(Exception):
+    pass
+
+
+class IncorrectEncodingError(Exception):
+    pass
+
+
+class IncorrectTimeoutError(Exception):
+    pass
+
+
+class IncorrectVerifyError(Exception):
+    pass
 
 
 class Config:
@@ -19,6 +52,10 @@ class Config:
         Args:
             path_to_config (pathlib.Path): Path to configuration.
         """
+        if not isinstance(path_to_config, pathlib.Path):
+            raise TypeError
+        self.path_to_config = path_to_config
+        self._extract_config_content()
 
     def _extract_config_content(self) -> ConfigDTO:
         """
@@ -27,11 +64,39 @@ class Config:
         Returns:
             ConfigDTO: Config values
         """
+        with open(self.path_to_config, 'r', encoding='utf-8') as config_file:
+            config_dict = config_file.read()
+            if not isinstance(config_dict, dict):
+                raise TypeError("inappropriate type of input arguments")
+        config = ConfigDTO(config_dict['seed_urls'],
+                           config_dict['total_articles_to_find_and_parse'],
+                           config_dict['headers'],
+                           config_dict['encoding'],
+                           config_dict['timeout'],
+                           config_dict['should_verify_certificate'],
+                           config_dict['headless_mode'])
+        return config
 
     def _validate_config_content(self) -> None:
         """
         Ensure configuration parameters are not corrupt.
         """
+        config = self._extract_config_content()
+        if not all(isinstance(url, str) for url in config.seed_urls) or ...:
+            raise IncorrectSeedURLError
+            # тут будет регулярка в if, когда разберусь что это и с чем это едят
+        if config.total_articles not in range(1, 151):
+            raise NumberOfArticlesOutOfRangeError
+        if not isinstance(config.total_articles, int) or config.total_articles < 0:
+            raise IncorrectNumberOfArticlesError
+        if not isinstance(config.headers, dict):
+            raise IncorrectHeadersError
+        if not isinstance(config.encoding, str):
+            raise IncorrectEncodingError
+        if not isinstance(config.timeout, int) or config.timeout <= 0 or config.timeout >= 60:
+            raise IncorrectTimeoutError
+        if not isinstance(config.should_verify_certificate, bool):
+            raise IncorrectVerifyError
 
     def get_seed_urls(self) -> list[str]:
         """
@@ -40,6 +105,8 @@ class Config:
         Returns:
             list[str]: Seed urls
         """
+        seed_urls = self._extract_config_content().seed_urls
+        return seed_urls
 
     def get_num_articles(self) -> int:
         """
@@ -48,6 +115,8 @@ class Config:
         Returns:
             int: Total number of articles to scrape
         """
+        num_articles = self._extract_config_content().total_articles
+        return num_articles
 
     def get_headers(self) -> dict[str, str]:
         """
@@ -56,6 +125,8 @@ class Config:
         Returns:
             dict[str, str]: Headers
         """
+        headers = self._extract_config_content().headers
+        return headers
 
     def get_encoding(self) -> str:
         """
@@ -64,6 +135,8 @@ class Config:
         Returns:
             str: Encoding
         """
+        encoding = self._extract_config_content().encoding
+        return encoding
 
     def get_timeout(self) -> int:
         """
@@ -72,6 +145,8 @@ class Config:
         Returns:
             int: Number of seconds to wait for response
         """
+        timeout = self._extract_config_content().timeout
+        return timeout
 
     def get_verify_certificate(self) -> bool:
         """
@@ -80,6 +155,8 @@ class Config:
         Returns:
             bool: Whether to verify certificate or not
         """
+        verify_certificate = self._extract_config_content().should_verify_certificate
+        return verify_certificate
 
     def get_headless_mode(self) -> bool:
         """
@@ -88,6 +165,8 @@ class Config:
         Returns:
             bool: Whether to use headless mode or not
         """
+        headless_mode = self._extract_config_content().headless_mode
+        return headless_mode
 
 
 def make_request(url: str, config: Config) -> requests.models.Response:
@@ -101,6 +180,13 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     Returns:
         requests.models.Response: A response from a request
     """
+    if not isinstance(url, str) or not isinstance(config, Config):
+        raise TypeError
+    response = requests.get(url,
+                            headers=Config.get_headers(),
+                            timeout=Config.get_timeout(),
+                            verify=Config.get_verify_certificate())
+    return response
 
 
 class Crawler:
@@ -206,12 +292,27 @@ def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
     Args:
         base_path (Union[pathlib.Path, str]): Path where articles stores
     """
+    try:
+        base_path.mkdir(exist_ok=False)
+    except FileExistsError:
+        if os.listdir(base_path):
+            base_path.rmdir()
+            base_path.mkdir()
+        else:
+            pass
+    except AttributeError:
+        if not isinstance(base_path, str):
+            raise TypeError
+        new_path = pathlib.Path(base_path)
+        new_path.mkdir()
 
 
 def main() -> None:
     """
     Entrypoint for scrapper module.
     """
+    configuration = Config(path_to_config=CRAWLER_CONFIG_PATH)
+    prepare_environment(ASSETS_PATH)
 
 
 if __name__ == "__main__":
